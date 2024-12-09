d65a382ec8f8f845a01ed1f589f0b5047461c8f7
laiscrz <lais.albino2003@gmail.com>
2024-12-09 14:57:44 -0300

    feat: created index page about Pizza

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using PizzasHub.Models;
+using PizzasHub.Services;
 
-namespace PizzasHub.Controllers;
-
-public class PizzaController : Controller
+namespace PizzasHub.Controllers
 {
+    public class PizzaController : Controller
+    {
+        private readonly IPizzaService _pizzaService;
 
+        public PizzaController(IPizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var pizzas = await _pizzaService.GetAllPizzasAsync();
+
+            return View(pizzas);
+        }
+    }
 }
